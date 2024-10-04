5460db72e23cbfa78d1c17813a714409402be16a
SardorBoboyev <boboyevsardor350@gmail.com>
2024-10-04 18:41:15 +0500

    this is comment

--- src/main/java/uz/sb/awswithdatabase/AwsWithDataBaseApplication.java
+++ src/main/java/uz/sb/awswithdatabase/AwsWithDataBaseApplication.java
@@ -2,12 +2,20 @@ package uz.sb.awswithdatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AwsWithDataBaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AwsWithDataBaseApplication.class, args);
     }
 
+
+    @GetMapping("/aws")
+    public String aws() {
+        return "This is my final aws, also connected to the database";
+    }
 }
