230ceec46f0d7bff1e3de387d4b8a66e9ccd1368
SardorBoboyev <boboyevsardor350@gmail.com>
2024-10-04 18:08:30 +0500

    Initial commit

---
+++ src/main/java/uz/sb/awswithdatabase/AwsWithDataBaseApplication.java
@@ -0,0 +1,13 @@
+package uz.sb.awswithdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsWithDataBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsWithDataBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/sb/awswithdatabase/AwsWithDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sb.awswithdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsWithDataBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
